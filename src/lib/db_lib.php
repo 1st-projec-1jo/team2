1daaf951004822ec9f41f1b6d732d862481b23f3
sangki-ko <chlt1@naver.com>
2024-10-11 18:00:05 +0900

    20241011 pop_up

--- src/lib/db_lib.php
+++ src/lib/db_lib.php
@@ -14,3 +14,25 @@ function my_db_conn() {
 
   return new PDO(MY_MARIADB_DSN, MY_MARIADB_USER, MY_MARIADB_PASSWORD, $option);
 }
+
+function my_pop_up_count_select(PDO $conn, array $arr_param) {
+  $sql =
+    " SELECT            "
+    ."  COUNT(*) cnt               "
+    ."  FROM             "
+    ."         sports_cal    "
+    ." WHERE            "
+    ."         deleted_at IS NULL "
+    ."       AND date = :date "
+    ;
+
+    $stmt = $conn->prepare($sql);
+    $result_flg = $stmt -> execute($arr_param);
+
+    if(!$result_flg){
+      throw new Exception("쿼리 실행 실패");
+    }
+
+    return $stmt->fetch()["cnt"];
+
+}
