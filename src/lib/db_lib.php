64ad09277fc8eac47b689ebbf03eb4ff387c857e
sangki-ko <chlt1@naver.com>
2024-10-13 14:58:04 +0900

    20241013 / delete

--- src/lib/db_lib.php
+++ src/lib/db_lib.php
@@ -15,6 +15,31 @@ function my_db_conn() {
   return new PDO(MY_MARIADB_DSN, MY_MARIADB_USER, MY_MARIADB_PASSWORD, $option);
 }
 
+function my_list_select(PDO $conn, array $arr_param){
+
+  $sql = 
+  " SELECT            "
+  ." *                "
+  ." FROM             "
+  ."         sports_cal    "
+  ." WHERE            "
+  ."         deleted_at IS NULL "
+  ."         AND date = :date "
+  ." ORDER BY         "
+  ."           created_at DESC, "
+  ."           id DESC          "
+  ;
+
+  $stmt = $conn->prepare($sql);
+  $result_flg = $stmt -> execute($arr_param);
+
+  if(!$result_flg){
+    throw new Exception("쿼리 실행 실패");
+  }
+
+  return $stmt -> fetchAll();
+}
+
 function my_pop_up_count_select(PDO $conn, array $arr_param) {
   $sql =
     " SELECT            "
@@ -35,4 +60,32 @@ function my_pop_up_count_select(PDO $conn, array $arr_param) {
 
     return $stmt->fetch()["cnt"];
 
+}
+
+
+function my_project_delete(PDO $conn, array $arr_param) {
+  $sql =
+    " UPDATE "
+    ." sports_cal "
+    ." SET "
+    ." updated_at = NOW() "
+    ." ,deleted_at = NOW() "
+    ." WHERE "
+    ." id = :id "
+  ;
+
+  $stmt = $conn->prepare($sql); // 쿼리 준비
+  $result_flg = $stmt->execute($arr_param); // 쿼리 실행
+
+  if(!$result_flg){
+    throw new Exception("쿼리 실행 실패");
+  }
+
+  $result_cnt = $stmt->rowCount();
+
+  if($result_cnt !== 1) {
+    throw new Exception("delete Count 이상");
+  }
+
+  return true;
 }
