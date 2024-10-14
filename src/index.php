170be0f905bf7cb28f069286d3f8ed5be8ebde44
LeeGyeongjinny <yikj555@gmail.com>
2024-10-14 12:18:03 +0900

    241014 index 추가

---
+++ src/index.php
@@ -0,0 +1,36 @@
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"]."/config.php"); 
+require_once(MY_PATH_DB_LIB);
+
+$conn = null;
+
+try {
+    $conn = my_db_conn();
+
+} catch(Throwable$th) {
+    require_once(MY_PATH_ERROR);
+    exit;
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/common.css">
+  <link rel="stylesheet" href="./css/index.css">
+  <title>Daily Routine</title>
+</head>
+<body>
+  <div class="index">
+    <h1>우주인의 데일리 루틴</h1>
+    <div class="index_btn">
+      <a href="/main_no.php"><button type="button" class="start">시작</button></a>
+    </div>
+  </div>
+</body>
+</html>
