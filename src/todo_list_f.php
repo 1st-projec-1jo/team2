b2d0e182483458fee34bcb9924c221d9ce2bf4bc
sangki-ko <chlt1@naver.com>
2024-10-18 11:00:25 +0900

    list_f 수정

--- src/todo_list_f.php
+++ src/todo_list_f.php
@@ -1,7 +1,6 @@
 <div class="container_l">
   <div class="date_btn">
-    <a href="/main.php
-    <?php if(isset($_GET["date"])) { ?>?year=<?php echo $ex[0] ?>&month=<?php echo $ex[1]?> <?php } ?>"><button type="button"><?php echo $date ?></button></a>
+    <a href="/main.php<?php if(isset($_GET["date"])) { ?>?year=<?php echo $ex[0] ?>&month=<?php echo $ex[1]?><?php } ?>"><button type="button"><?php echo $date ?></button></a>
   </div>
   <hr>
   <br>
